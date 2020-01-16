3498eb1d582deafa50543238882e5fe9d1a1ea79
tharindu2003 <tharindu2003@gmail.com>
2020-01-15 20:35:46 -0500

    recursive algorithm working

--- src/Other.java
+++ src/Other.java
@@ -21,7 +21,10 @@ public class Other {
     }
 
     public static void main(String[] args){
-
+        int[]a = new int[]{1,2,3};
+        int[]b = a.clone();
+        b[0] = 5;
+        System.out.println(a[0]);
     }
 
 }
